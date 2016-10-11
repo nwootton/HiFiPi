142d7e6deb5fa7978cfccdbce3211ea67559b444
nwootton <woottonn@ostml0253356.bskyb.com>
2016-10-11 13:08:23 +0100

    Initial version of single button press code

---
+++ button_test_v1.py
@@ -0,0 +1,19 @@
+
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+button = 4
+
+GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
+
+while True:
+  button_state = GPIO.input(button)
+  if button_state == GPIO.HIGH:
+    print ("HIGH")
+  else:
+    print ("LOW")
+
+    time.sleep(0.5)
