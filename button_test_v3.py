0865a88d3189711d4e05c00683d45b67043247b1
Nick Wootton <nick@nwootton.com>
2016-10-14 18:02:51 +0100

    Trying the event based detection

--- button_test_v3.py
+++ button_test_v3.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 # Assumes switch is connected to GND - so uses PULL UP
 
+import subprocess
 import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-button = 27 #Play/pause pin
+#Define GPIO Pins that have buttons
+play_pause = 27     #Play/pause mpc toggle
 
-GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(play_pause, GPIO.IN, GPIO.PUD_UP)
 
-GPIO.add_event_detect(button, GPIO.RISING)  # add rising edge detection on a button
+GPIO.add_event_detect(play_pause, GPIO.RISING)  # add rising edge detection on a button
 
-if GPIO.event_detected(button):
+if GPIO.event_detected(play_pause):
     print('Button pressed')
+    subprocess.call(['mpc', 'toggle' ])
