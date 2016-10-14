06b3e6b939919879d2a2ab1b825d78918b1f9b7b
Nick Wootton <nick@nwootton.com>
2016-10-14 18:06:57 +0100

    Nope - no idea here, just playing

--- button_test_v3.py
+++ button_test_v3.py
@@ -8,13 +8,15 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+#Define callbacks for event detection
+def play_pause_toggle(channel):
+    print ("Play/Pause")
+    subprocess.call(['mpc', 'toggle' ])
+
 #Define GPIO Pins that have buttons
 play_pause = 27     #Play/pause mpc toggle
 
 GPIO.setup(play_pause, GPIO.IN, GPIO.PUD_UP)
 
 GPIO.add_event_detect(play_pause, GPIO.RISING)  # add rising edge detection on a button
-
-if GPIO.event_detected(play_pause):
-    print('Button pressed')
-    subprocess.call(['mpc', 'toggle' ])
+GPIO.add_event_callback(play_pause, play_pause_toggle) #Call function when detected
